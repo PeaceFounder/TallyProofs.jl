a56e24eafca81532d2e7f0a7fea018994b482b29
Janis Erdmanis <janiserdmanis@protonmail.ch>
2025-02-04 02:04:03 +0200

    fixing codecov

--- src/pedersen.jl
+++ src/pedersen.jl
@@ -4,74 +4,74 @@ using SigmaProofs.Verificatum: ProtocolSpec, ro_prefix
 using SigmaProofs.Parser: Tree, encode
 import SigmaProofs: prove, verify, proof_type
 
-struct PedersenCommitment{G <: Group} <: Proposition
-    h::G
-    g::G
-    C::G
-end
+# struct PedersenCommitment{G <: Group} <: Proposition
+#     h::G
+#     g::G
+#     C::G
+# end
 
-struct PedersenProof{G <: Group} <: Proof
-    A::G
-    s::BigInt
-    p::BigInt
-end
+# struct PedersenProof{G <: Group} <: Proof
+#     A::G
+#     s::BigInt
+#     p::BigInt
+# end
 
-proof_type(::Type{PedersenCommitment{G}}) where G <: Group = PedersenProof{G}
+# proof_type(::Type{PedersenCommitment{G}}) where G <: Group = PedersenProof{G}
 
-function commit(g::G, h::G, m::Integer, r::Integer) where G <: Group
+# function commit(g::G, h::G, m::Integer, r::Integer) where G <: Group
     
-    C = h^r * g^m
+#     C = h^r * g^m
 
-    return PedersenCommitment(h, g, C)
-end
+#     return PedersenCommitment(h, g, C)
+# end
 
-function commit(g::G, h::G, m::Integer, r::Integer, verifier::Verifier; roprg = gen_roprg()) where G <: Group
+# function commit(g::G, h::G, m::Integer, r::Integer, verifier::Verifier; roprg = gen_roprg()) where G <: Group
 
-    proposition = commit(g, h, m, r)
-    proof = prove(proposition, verifier, m, r; roprg)
+#     proposition = commit(g, h, m, r)
+#     proof = prove(proposition, verifier, m, r; roprg)
 
-    return Simulator(proposition, proof, verifier)
-end
+#     return Simulator(proposition, proof, verifier)
+# end
 
-function challenge(verifier::ProtocolSpec{G}, proposition::PedersenCommitment{G}, A::G) where G <: Group
+# function challenge(verifier::ProtocolSpec{G}, proposition::PedersenCommitment{G}, A::G) where G <: Group
     
-    (; h, g, C) = proposition
+#     (; h, g, C) = proposition
 
-    ρ = ro_prefix(verifier)
+#     ρ = ro_prefix(verifier)
 
-    tree = Tree((h, g, C, A))
+#     tree = Tree((h, g, C, A))
 
-    prg = PRG(verifier.prghash, [ρ..., encode(tree)...])
+#     prg = PRG(verifier.prghash, [ρ..., encode(tree)...])
 
-    return rand(prg, 2:order(G) - 1)
-end
+#     return rand(prg, 2:order(G) - 1)
+# end
 
-function prove(proposition::PedersenCommitment{G}, verifier::Verifier, m::Integer, r::Integer; roprg = gen_roprg()) where G <: Group
+# function prove(proposition::PedersenCommitment{G}, verifier::Verifier, m::Integer, r::Integer; roprg = gen_roprg()) where G <: Group
 
-    (; h, g, C) = proposition
+#     (; h, g, C) = proposition
 
-    w = rand(roprg(:w), 2:order(G)-1)
-    z = rand(roprg(:z), 2:order(G)-1)
+#     w = rand(roprg(:w), 2:order(G)-1)
+#     z = rand(roprg(:z), 2:order(G)-1)
 
-    A = h^w * g^z
+#     A = h^w * g^z
 
-    c = challenge(verifier, proposition, A)
+#     c = challenge(verifier, proposition, A)
 
-    s = w + c * r
-    p = z + c * m
+#     s = w + c * r
+#     p = z + c * m
 
-    return PedersenProof(A, s, p)
-end
+#     return PedersenProof(A, s, p)
+# end
 
-function verify(proposition::PedersenCommitment{G}, proof::PedersenProof{G}, verifier::Verifier) where G <: Group
+# function verify(proposition::PedersenCommitment{G}, proof::PedersenProof{G}, verifier::Verifier) where G <: Group
 
-    (; h, g, C) = proposition
-    (; A, s, p) = proof
+#     (; h, g, C) = proposition
+#     (; A, s, p) = proof
 
-    c = challenge(verifier, proposition, A)
+#     c = challenge(verifier, proposition, A)
 
-    return A * C^c == h^s * g^p
-end
+#     return A * C^c == h^s * g^p
+# end
 
 struct LambdaCommitment{G<:Group} <: Proposition
     h::G
